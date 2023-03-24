90d09efa5310c23f66922f304f686659ca612bbb
melihsahtulek <melihsahtulek@gmail.com>
2023-03-25 01:51:02 +0300

    add: Type tanımlama

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,32 @@ const person: {
 };
 
 const numbers: number[] = [1, 2];
+
+function doSomething(): string {
+  return "Write something else!";
+}
+
+function calculate(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+// console.log(calculate(10, 25));
+
+function repeat(name: string, repeatValue: number): void {
+  for (let i = 0; i < repeatValue; i++) {
+    console.log(`${i + 1}. ${name}`);
+  }
+}
+
+repeat("Melihşah", 5);
+
+type Car = {
+  brand: string;
+  colour?: number;
+};
+
+const car: Car = {
+  brand: "Mazda",
+};
+
+console.log(car);
