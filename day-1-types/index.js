d851c57089294e3a7cabaf91c1de8dd824257ada
melihsahtulek <melihsahtulek@gmail.com>
2023-05-11 23:21:23 +0300

    add: init files

---
+++ day-1-types/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var faker_1 = require("@faker-js/faker");
+var fullName = faker_1.faker.name.fullName();
+console.log(fullName);
---
+++ day-1-types/index.ts
@@ -0,0 +1,5 @@
+import { faker } from "@faker-js/faker";
+
+let fullName: string = faker.name.fullName();
+
+console.log(fullName);
